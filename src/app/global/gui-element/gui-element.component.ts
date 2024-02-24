9e33eb89f14c8e2a4bd1557fb8033a9337088d1a
MangoButtermilch <aceofbeats883@gmail.com>
2024-02-24 12:57:59 +0100

    update gui element position by their transform style (for saving and loading)

--- src/app/global/gui-element/gui-element.component.ts
+++ src/app/global/gui-element/gui-element.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { GuiObject } from 'src/models/models';
+import { CdkDragEnd } from '@angular/cdk/drag-drop';
+import { AfterViewInit, Component, ElementRef, Input } from '@angular/core';
+import { GuiObject, Vector2 } from 'src/models/models';
 
 @Component({
   selector: 'app-gui-element',
@@ -10,7 +11,25 @@ export class GuiElementComponent {
 
   @Input() guiElement: GuiObject;
 
+  constructor(private elementRef: ElementRef) { }
+
+
   public get transformStyle(): string {
     return `transform: translate3d(${this.guiElement.position.x}px, ${this.guiElement.position.y}px, 0px);`
+  }
+
+  public updatePosition(eventData: CdkDragEnd): void {
+    const styleDiv = this.elementRef.nativeElement.querySelector(".node");
+    const style = styleDiv.style;
+    const transform = style.transform;
+
+    const regex = /translate3d\((-?\d+)px, (-?\d+)px, (-?\d+)px\)/;
+    const matches = transform.match(regex);
+    if (!matches) return;
+
+    const x = parseInt(matches[1]);
+    const y = parseInt(matches[2]);
+
+    this.guiElement.position = { x: x, y: y };
   }
 }
